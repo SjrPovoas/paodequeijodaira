8d4ff6893128cf1b3bcb5e1b18749fbd9b430733
Silvio Póvoas Júnior <87617524+SjrPovoas@users.noreply.github.com>
2026-01-16 12:14:25 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,33 +58,31 @@ export default function Home() {
         <meta name="googlebot" content="index,follow" />
         <meta name="google-site-verification" content="rj9-yKQenuTL7WznZzLhnZhRRqalrW8B9ptmhuewFiA" />
 
-        {/* OPEN GRAPH */}
+        {/* Meta Tags para WhatsApp / Facebook (Open Graph) */}
         <meta property="og:locale" content="pt_BR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.facebook.com/paodeuqiejodaira.iraleide" />
         <meta property="og:title" content="Pão de Queijo da Irá" />
         <meta property="og:description" content="O melhor pão de queijo congelado e caseiro da Cidade Ocidental. Temos pacote com 20 pães de queijo congelado e pacote com 1 kg de pão de queijo congelado." />
         <meta property="og:image" content="https://paodequeijodaira.vercel.app/logo-paodequeijodaira.jpg" />
         <meta property="og:site_name" content="Pão de Queijo da Irá" />
-        <meta property="og:url" content="https://www.facebook.com/paodeuqiejodaira.iraleide" />
+        <meta property="og:image" content="https://paodequeijodaira.vercel.app/logo-paodequeijodaira.jpg" />
+        <meta property="og:image:secure_url" content="https://paodequeijodaira.vercel.app/logo-paodequeijodaira.jpg" />
+        <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:type" content="website" />
+        <meta property="og:image:height" content="630" />        <meta property="og:type" content="website" />
         <meta property="fb:pages" content="359950968036532" />
 
-        {/* Twitter Card */}
+        {/* Twitter / X */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@PaodQueijodaIra" />
         <meta name="twitter:creator" content="@PaodQueijodaIra" />
         <meta name="twitter:title" content="Pão de Queijo da Irá" />
         <meta name="twitter:description" content="O melhor pão de queijo congelado e caseiro da Cidade Ocidental. Temos pacote com 20 pães de queijo congelado e pacote com 1 kg de pão de queijo congelado." />
         <meta name="twitter:image" content="https://paodequeijodaira.vercel.app/logo-paodequeijodaira.jpg" />
-        <meta name="twitter:url" content="https://x.com/PaodQueijodaIra" />
+        <meta name="twitter:url" content="https://x.com/PaodQueijodaIra" />    
 
         <title>Pão de Queijo da Irá | O melhor pão de queijo congelado e caseiro da Cidade Ocidental</title>
-        <link rel="icon" sizes="32x32" href="/favicon.png" />
-        <link rel="icon" sizes="192x192" href="https://paodequeijodaira.vercel.app/logo-paodequeijodaira.jpg" />
-        <link rel="apple-touch-icon" type="image/x-icon" href="https://paodequeijodaira.vercel.app/favicon.png" />
-        <link rel="shortcut icon" type="image/x-icon" href="https://paodequeijodaira.vercel.app/favicon.ico" />
-        <link rel="mask-icon" href="https://paodequeijodaira.vercel.app/favicon.png" />
 
         {/* SCRIPTS EXTERNOS */}
         <script src="https://cdn.tailwindcss.com"></script>
