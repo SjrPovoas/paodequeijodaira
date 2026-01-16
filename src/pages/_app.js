7d29f525a52a7daff7fb32620d03562027c89df2
Silvio Póvoas Júnior <87617524+SjrPovoas@users.noreply.github.com>
2026-01-16 09:46:21 -0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,27 @@
-import '../globals.css'
+import Head from 'next/head';
+import '../styles/globals.css'; // Seus estilos globais
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* Ícone padrão para navegadores */}
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        
+        {/* Ícone para iPhones/iOS (Home Screen) */}
+        <link rel="apple-touch-icon" href="/logo-paodequeijodaira.jpg" />
+        
+        {/* Melhora a compatibilidade com navegadores antigos */}
+        <link rel="shortcut icon" href="/favicon.ico" />
+        
+        {/* Define o título padrão caso a página não tenha um */}
+        <title>Pão de Queijo da Irá</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
