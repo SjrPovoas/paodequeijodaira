18da6711ec7ef2575edf2b74ddae67eb3475df87
Silvio Póvoas Júnior <87617524+SjrPovoas@users.noreply.github.com>
2026-01-16 08:35:11 -0300

    Update termos.js

--- src/pages/termos.js
+++ src/pages/termos.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'; // Corrigido: Importado useEffect
 import Head from 'next/head';
 
 export default function Termos() {
