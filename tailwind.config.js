50e142020f82f7177b26b658d5d793c4a36b4827
Silvio Póvoas Júnior <87617524+SjrPovoas@users.noreply.github.com>
2026-01-20 19:17:46 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Abrange tudo dentro de src (mais seguro)
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        // Você pode dar um nome para o seu laranja oficial aqui
         'ira-orange': '#ea580c',
       },
     },
