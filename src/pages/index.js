dae08d31d1ce943d4dfb08a9437745d1a0e9b967
Silvio Póvoas Júnior <87617524+SjrPovoas@users.noreply.github.com>
2026-01-29 16:20:51 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -101,7 +101,10 @@ export default function Home() {
       <header className="border-b border-gray-100 py-4 px-6 sticky top-0 bg-white/95 backdrop-blur-md z-[100]">
         <div className="max-w-7xl mx-auto flex justify-between items-center">
           {/* LOGO */}
-          <Link href="/"><img src="/logo-paodequeijodaira.jpg" alt="Logo" className="h-12 md:h-16 w-auto" /></Link>
+          <Link href="/"><img src="/logo-paodequeijodaira.jpg" alt="Logo" className="h-12 md:h-16 w-auto cursor-pointer" /></Link>
+          <button onClick={toggleMenu} className="lg:hidden text-3xl text-orange-600 relative z-[110] focus:outline-none">
+            <i className={isMenuOpen ? "bi bi-x-lg" : "bi bi-list"}></i>
+          </button>
 
           {/* NAVEGAÇÃO DESKTOP */}
           <nav className="hidden md:flex space-x-6 text-[10px] font-bold uppercase tracking-widest items-center">
@@ -111,10 +114,10 @@ export default function Home() {
             <Link href="/loja" className="text-orange-600 border border-orange-600 px-4 py-2 rounded-full hover:bg-orange-600 hover:text-white transition-all">LOJA LIFESTYLE</Link>
             <button onClick={() => setIsModalOpen(true)} className="bg-orange-600 text-white px-8 py-4 font-black uppercase tracking-widest text-xs shadow-lg hover:scale-105 transition-all">Pedir Agora</button>
           </nav>
-            {/* Botão Hambúrguer Mobile */} 
+            {/* Botão Hambúrguer Mobile 
             <button onClick={toggleMenu} className="lg:hidden text-3xl text-orange-600 relative z-[110]">
               <i className={isMenuOpen ? "bi bi-x-lg" : "bi bi-list"}></i>
-            </button>
+            </button> */}
           
 
          {/* NAVEGAÇÃO MOBILE */}     
